5993ac7fae85378eb84f30355c6361f47a6901d5
David Marcos <ddragondavi@gmail.com>
2024-04-08 21:19:15 -0300

    implementation crud categories

--- src/apps/categories/categoryController.js
+++ src/apps/categories/categoryController.js
@@ -7,23 +7,120 @@ class CategoryControler{
         res.render("admin/category/form")
     }
 
+    static findAll(req,res){
+
+        Category.findAll({row:true})
+            .then( 
+                categories => res.render("admin/category/categories",{categories})
+            )
+            .catch(
+                error => res.status(500).send("Não foi possivel conclui operação !!!")
+            )
+
+    }
+
     static create(req,res){
 
         const titulo =  req.body.titulo
 
         if (titulo){
-            Category.create({
+            Category.create(
+                {
                     name: titulo,
                     slug: Slugify(titulo)
                 }
-            ).then(() => res.redirect("/"))
-            .catch( error => res.status(500).send("Não foi possivel conclui operação !!!") )
+            ).then(
+                () => res.redirect("/admin/categories")
+            )
+            .catch( 
+                error => res.status(500).send("Não foi possivel conclui operação !!!") 
+            )
 
         }else{
             res.redirect("admin/category/form")
 
         }
     }
+
+    static updateForm(req, res){
+
+        const id = req.params.id
+
+        if(id){
+            Category.findOne(
+                { where:{ id: id } }
+            )
+            .then(
+                category => res.render("admin/category/formUpdate",{category})
+            )
+            .catch( e =>
+                res.redirect("admin/categories")
+            )
+            
+        }else{
+            res.status(404).send("Não foi possivel realizar operação !!!")
+        }
+
+    }
+
+    static update(req, res) {
+        const id = req.params.id;
+        const titulo = req.body.titulo;
+
+        console.log("here", id, "-", titulo)
+    
+        if (id && titulo) {
+            Category.update(
+                {
+                    name: titulo,
+                    slug: Slugify(titulo)
+                }, 
+                {
+                    where: {id: id}
+
+                }
+            )
+            .then(
+                () => res.redirect("/admin/categories")
+            )
+            .catch(
+                error => res.status(500).send("Não foi possível concluir a operação!!!")
+            )
+
+        } else {
+            res.redirect("admin/category/form")
+        }
+    }
+
+    static delete( req, res){
+        const id = req.params.id
+
+        if(id){
+            Category.findOne(
+                    {where:{ id: id} }
+                )
+                .then(
+                    category =>{
+                        if (category){
+                            Category.destroy(
+                                {where:{ id: id}}
+                            )
+                            .then(
+                                () => res.redirect("/admin/categories")
+                            )
+                        }else{
+                            res.status(404).send("Categoria não encontrada !!!")
+                        }
+                    }
+                )
+                .catch(
+                    error => res.status(404).send("Categoria não encontrada !!!")
+                )
+        }else{
+            res.status(404).send("Não foi possivel realizar operação !!!")
+        }
+    }
+    
 }
 
 module.exports = CategoryControler
--- src/apps/categories/categoryRouter.js
+++ src/apps/categories/categoryRouter.js
@@ -3,7 +3,11 @@ const CategoryController = require("./categoryController")
 
 const categoriesRouters = express.Router()
 
+categoriesRouters.get("/admin/categories", CategoryController.findAll)
 categoriesRouters.get("/admin/categories/new", CategoryController.form)
+categoriesRouters.get("/admin/categories/delete/:id", CategoryController.delete)
+categoriesRouters.get("/admin/categories/update/:id", CategoryController.updateForm)
+categoriesRouters.post("/admin/categories/update/:id", CategoryController.update)
 categoriesRouters.post("/admin/categories/save", CategoryController.create)
 
 module.exports = categoriesRouters
